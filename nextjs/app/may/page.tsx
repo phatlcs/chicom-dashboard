ac7f97229030849c654fbb01a17f83a554eede76
neo <neo@coderpush.com>
2026-06-07 16:43:03 +0700

    feat: add /may route to display may.html dashboard

---
+++ nextjs/app/may/page.tsx
@@ -0,0 +1,8 @@
+export default function MayPage() {
+  return (
+    <iframe
+      src="/may.html"
+      style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', border: 'none', zIndex: 1 }}
+    />
+  )
+}
